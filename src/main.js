e1ced4227b93d456bc87155c8a563a00ec267122
Ruan Ripardo <ruanpeixotorip@gmail.com>
2023-12-13 16:18:41 -0300

    feat: vue router

--- src/main.js
+++ src/main.js
@@ -3,4 +3,15 @@ import './style.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import App from './App.vue'
 
+// const routes = [
+//     { path: '/', component: Home },
+//     { path: '/about', component: About },
+// ]
+
+// const router = VueRouter.createRouter({
+//     history: VueRouter.createWebHashHistory(),
+// });
+  
+// app.use(router);
+
 createApp(App).mount('#app')
